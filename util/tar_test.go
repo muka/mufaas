fa9fc843ed034fc08f198decb28f5a567ccc9ad7
Luca Capra <luca.capra@gmail.com>
2017-12-25 09:21:55 +0100

    added util test

---
+++ util/tar_test.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func TestCreateArchive(t *testing.T) {
+
+	root, err := ioutil.TempDir("", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(root)
+
+	log.Debugf("Created tmp dir %s", root)
+
+	dir, err := ioutil.TempDir(root, "content")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(dir, "file.txt"), []byte{}, 0x700)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = CreateArchive(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = os.Stat(dir + ".tar")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
