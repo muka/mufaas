f253c8a690c8f274cfd22e8274597b7a6ad905cc
Luca Capra <luca.capra@gmail.com>
2017-12-29 06:58:14 +0100

    log archive ops

--- util/tar.go
+++ util/tar.go
@@ -1,39 +1,51 @@
 package util
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
 	"github.com/mholt/archiver"
 	"github.com/monochromegane/go-gitignore"
+	log "github.com/sirupsen/logrus"
 )
 
 func CreateTar(dir string) error {
 
-	list := []string{dir}
+	log.Debugf("Create archive for %s", dir)
+
+	list := []string{}
 
 	ignfile := filepath.Join(dir, ".dockerignore")
-	if _, err := os.Stat(ignfile); err == nil {
 
-		gitignore, err := gitignore.NewGitIgnore(ignfile, dir)
+	var ignore gitignore.IgnoreMatcher
+	if _, err := os.Stat(ignfile); err == nil {
+		log.Debugf("Found %s", ignfile)
+		ignore, err = gitignore.NewGitIgnore(ignfile)
 		if err != nil {
 			return err
 		}
+	}
 
-		list := []string{}
-		err = filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+	err := filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
 
-			if !gitignore.Match(path, f.IsDir()) {
+		if ignore != nil {
+			if !ignore.Match(path, f.IsDir()) {
 				list = append(list, path)
 			}
-
-			return nil
-		})
-
-		if err != nil {
-			return err
+		} else {
+			list = append(list, path)
 		}
 
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if len(list) == 0 {
+		return errors.New("Cannot create an empty archive")
 	}
 
 	return archiver.Tar.Make(dir+".tar", list)
