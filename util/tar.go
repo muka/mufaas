c9a78fa4ea1c45183bbe06b19dc9a340290aa866
Luca Capra <luca.capra@gmail.com>
2017-12-29 06:08:36 +0100

    added extract and gitignore support

--- util/tar.go
+++ util/tar.go
@@ -1,24 +1,48 @@
 package util
 
 import (
-	"github.com/jhoonb/archivex"
+	"os"
+	"path/filepath"
+
+	"github.com/mholt/archiver"
+	"github.com/monochromegane/go-gitignore"
 )
 
+func CreateTar(dir string) error {
+
+	list := []string{dir}
+
+	ignfile := filepath.Join(dir, ".dockerignore")
+	if _, err := os.Stat(ignfile); err == nil {
+
+		gitignore, err := gitignore.NewGitIgnore(ignfile, dir)
+		if err != nil {
+			return err
+		}
+
+		list := []string{}
+		err = filepath.Walk(dir, func(path string, f os.FileInfo, err error) error {
+
+			if !gitignore.Match(path, f.IsDir()) {
+				list = append(list, path)
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+	}
+
+	return archiver.Tar.Make(dir+".tar", list)
+}
+
+func ExtractTar(archive, dst string) error {
+	return archiver.Tar.Open(archive, dst)
+}
+
 func CreateArchive(dir string) error {
-
-	tar := new(archivex.TarFile)
-	err := tar.Create(dir)
-	if err != nil {
-		return err
-	}
-	err = tar.AddAll(dir, false)
-	if err != nil {
-		return err
-	}
-	err = tar.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return archiver.Tar.Make(dir+".tar", []string{dir})
 }
--- util/tar_test.go
+++ util/tar_test.go
@@ -11,40 +11,102 @@ import (
 
 func TestCreateArchiveNonExistantDir(t *testing.T) {
 
-	err := CreateArchive("/foo")
+	err := CreateTar("/foo")
 	if err == nil {
 		t.Fatal("Expeceted to fail creation")
 	}
 }
 
-func TestCreateArchive(t *testing.T) {
+func createTar() (string, string, error) {
 
 	root, err := ioutil.TempDir("", "test")
 	if err != nil {
-		t.Fatal(err)
+		return root, "", err
 	}
-	defer os.Remove(root)
 
 	log.Debugf("Created tmp dir %s", root)
 
 	dir, err := ioutil.TempDir(root, "content")
 	if err != nil {
-		t.Fatal(err)
+		return root, "", err
 	}
 
-	err = ioutil.WriteFile(filepath.Join(dir, "file.txt"), []byte{}, 0x700)
+	dir2, err := ioutil.TempDir(dir, "subdir")
 	if err != nil {
-		t.Fatal(err)
+		return root, "", err
 	}
 
-	err = CreateArchive(dir)
+	err = ioutil.WriteFile(filepath.Join(dir, "file.txt"), []byte{}, 0644)
 	if err != nil {
-		t.Fatal(err)
+		return root, "", err
 	}
 
+	err = ioutil.WriteFile(filepath.Join(dir2, "file2.txt"), []byte{}, 0644)
+	if err != nil {
+		return root, "", err
+	}
+
+	// ignore: /subdir/file.tmp
+	err = ioutil.WriteFile(filepath.Join(dir2, "file.tmp"), []byte{}, 0644)
+	if err != nil {
+		return root, "", err
+	}
+
+	// ignore: /file.tmp
+	err = ioutil.WriteFile(filepath.Join(dir, "file.tmp"), []byte{}, 0644)
+	if err != nil {
+		return root, "", err
+	}
+
+	err = ioutil.WriteFile(filepath.Join(dir, ".dockerignore"), []byte("file.tmp\n"), 0644)
+	if err != nil {
+		return root, "", err
+	}
+
+	err = CreateTar(dir)
+	if err != nil {
+		return root, "", err
+	}
 	_, err = os.Stat(dir + ".tar")
 	if err != nil {
+		return root, "", err
+	}
+
+	return root, dir + ".tar", err
+}
+
+func TestCreateTar(t *testing.T) {
+	root, _, err := createTar()
+	defer os.Remove(root)
+	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestExtractTar(t *testing.T) {
+	root, tar, err := createTar()
+	defer os.Remove(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dir, err := ioutil.TempDir("", "test")
+	defer os.Remove(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = ExtractTar(tar, dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := os.Stat(filepath.Join(dir, "subdir", "file.tmp")); err == nil {
+		t.Fatal("file.tmp should have been ignored")
+	}
+
+	if _, err := os.Stat(filepath.Join(dir, "file.tmp")); err == nil {
+		t.Fatal("file.tmp should have been ignored")
 	}
 
 }
